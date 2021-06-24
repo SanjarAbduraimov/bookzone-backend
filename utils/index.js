06646b37564d24efe21cb5ec47f564ec0f0e8a9c
Alisher778 <alisher.musurmonov89@gmail.com>
2021-06-24 19:08:28 +0500

    :bug: Fix 401 issue

--- utils/index.js
+++ utils/index.js
@@ -13,42 +13,49 @@ exports.validateToken = token => {
   catch (err) {
     return {};
   }
-
 }
 
 exports.currentUser = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   const validToken = token ? this.validateToken(token) : {};
 
-  try {
-    const user = await Admin.findById(validToken._id);
-    if (user) {
-      req.locals = { ...req.locals, _id: user._id };
-      next();
-    } else {
-      res.status(403).json({ success: false, error: 'You are not authorized' });
-    }
+  if (validToken._id) {
+    try {
+      const user = await Admin.findById(validToken._id);
+      if (user) {
+        req.locals = { ...req.locals, _id: user._id };
+        next();
+      } else {
+        res.status(403).json({ success: false, error: 'You are not authorized' });
+      }
 
-  } catch (error) {
-    console.log(error)
+    } catch (error) {
+      console.log(error)
+      res.status(401).json({ success: false, error: 'You are not authenticated' });
+    }
+  } else {
     res.status(401).json({ success: false, error: 'You are not authenticated' });
   }
 }
 exports.isAdmin = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  console.log(this.validateToken(token));
   const validToken = token ? this.validateToken(token) : {};
-  try {
-    const admin = await Admin.findById(validToken._id);
-    if (admin) {
-      req.locals = { ...req.locals, _id: admin._id, role: 'admin' };
-      next();
-    } else {
-      res.status(403).json({ success: false, error: 'You are not authorized' });
-    }
+  if (validToken._id) {
+    try {
+      const admin = await Admin.findById(validToken._id);
+      if (admin) {
+        req.locals = { ...req.locals, _id: admin._id, role: 'admin' };
+        next();
+      } else {
+        res.status(403).json({ success: false, error: 'You are not authorized' });
+      }
 
-  } catch (error) {
-    console.log(error)
+    } catch (error) {
+      console.log(error)
+      res.status(401).json({ success: false, error: 'You are not authenticated' });
+    }
+  } else {
     res.status(401).json({ success: false, error: 'You are not authenticated' });
   }
+
 }
